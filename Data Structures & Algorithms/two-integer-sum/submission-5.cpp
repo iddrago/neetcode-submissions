9f698cd24b4aed9472e72c787223146d7f6f1832
iddrago <69974698+iddrago@users.noreply.github.com>
2026-07-27 23:42:12 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<pair<int,int>> arr;
+        for(int i=0;i<n;i++){
+            arr.push_back({nums[i],i});
+        }
+        sort(arr.begin(),arr.end());
+
+        int l=0;
+        int r=n-1;
+        while(l<r ){
+        if(arr[l].first+arr[r].first==target){ return {
+        min(arr[l].second, arr[r].second),
+        max(arr[l].second, arr[r].second)
+    };}
+
+        else if(arr[l].first+arr[r].first<target){
+            l++;
+        }
+        else r--;
+        }
+
+        
+    }
+};
