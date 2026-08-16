2f6d0c15f53784ce5b894d389188f8fbe0cb9bf7
iddrago <69974698+iddrago@users.noreply.github.com>
2026-08-16 22:35:23 +0530

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr)
+            return;
+
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        
+        ListNode* second = slow->next;
+        slow->next = nullptr;
+
+        ListNode* prev = nullptr;
+
+        while(second != nullptr) {
+            ListNode* next = second->next;
+            second->next = prev;
+            prev = second;
+            second = next;
+        }
+
+        
+        ListNode* first = head;
+        second = prev;
+
+        while(second != nullptr) {
+            ListNode* firstNext = first->next;
+            ListNode* secondNext = second->next;
+
+            first->next = second;
+            second->next = firstNext;
+
+            first = firstNext;
+            second = secondNext;
+        }
+    }
+};
