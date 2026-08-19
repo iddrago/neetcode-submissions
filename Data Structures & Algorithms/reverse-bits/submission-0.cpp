31841b831749e851d9fe2a71704679f918c38098
iddrago <69974698+iddrago@users.noreply.github.com>
2026-08-19 21:53:26 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int ans = 0;
+        int mask=1;
+        for(int i=0;i<32;i++){
+            if((mask&n)!=0){
+                ans +=1<<31-i;
+            }
+            mask<<=1;
+        }
+        return ans;
+    }
+};
