b75acc07e78ad21fac432a34f437a1e45819c33f
iddrago <69974698+iddrago@users.noreply.github.com>
2026-09-16 10:56:38 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1;i<n;i++){
+            int take = nums[i]+prev2;
+            int notTake = 0+prev;
+            int curi = max(take,notTake);
+            prev2=prev;
+            prev=curi;
+        }
+        return prev;
+    }
+};
